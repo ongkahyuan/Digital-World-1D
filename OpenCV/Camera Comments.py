dba3fcbeca5b698d2b1fe2297a065bb7f996a260
Jed <61384846+jetwreck@users.noreply.github.com>
2020-03-18 13:39:36 +0800

    OpenCV Brightness Detection
    Im Done with the code bwahahahaha

--- OpenCV/Camera Comments.py
+++ OpenCV/Camera Comments.py
@@ -1,16 +1,26 @@
 import numpy as numpy
 import cv2
 
-class room_brightness:
-    def check_brightness():
+class room_brightness: # Creating class
+    def check_brightness(): #Creating function within class
+        #------------------------Reading image--------------------------------------
         camera_index = 0 # Video camera number 0
         cap = cv2.VideoCapture(camera_index, cv2.CAP_DSHOW) #cap as an object, Setting resolution using cap_dshow
-        ret, img = cap.read()
+        ret, img = cap.read() #returning image as reading cap Object
+        #------------------------ My Test Image-------------------------------------
+        # img = cv2.imread('CC.png')
+
         img_dot = img
+        #-----Converting image to LAB Color model----------------------------------- 
         lab= cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
+        #-----Splitting the LAB image to different channels-------------------------
         l, a, b = cv2.split(lab)
+
+        #-----Finding average lightness level in image by fixing some points--------
         y,x,z = img.shape #height, width of image
-#         print('>> Image Dimension => X:{}, Y:{}'.format(x,y))
+        # print('>> Image Dimension => X:{}, Y:{}'.format(x,y))                             # Prints the resolution of image
+        #Deciding some dynamic points on image for checking light intensity
         l_blur = cv2.GaussianBlur(l, (11, 11), 5)
         maxval = []
         count_percent = 10 #percent of total image
@@ -22,14 +32,20 @@ class room_brightness:
                 for j in range(1, y-1):
                     if j%column_percent == 0:
                         pix_cord = (i,j)
+
                         cv2.circle(img_dot, (int(i), int(j)), 5, (0, 255, 0), 2)
                         img_segment = l_blur[i:i+3, j:j+3]
                         (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(img_segment)
                         maxval.append(maxVal)
+
         avg_maxval = round(sum(maxval) / len(maxval))
-#         print('>> Total points: {}'.format(len(maxval)))
-#         print('>> Average Brightness: {}'.format(avg_maxval))
-        return avg_maxval
+        # print('>> Total points: {}'.format(len(maxval)))                                  # Prints total points of data taken from image
+        # print('>> Average Brightness: {}'.format(avg_maxval))                             # Prints average brightness of points of data taken
+        return avg_maxval # Returns the brightness level
+        cv2.namedWindow('img', cv2.WINDOW_AUTOSIZE) #This and below shows the picture taken
+        cv2.imshow('img', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 print(check_brightness())
 
@@ -40,5 +56,5 @@ def Check_light_state():
     elif check_brightness() < 50:
         a = False
         return "Lights in the Room are 'OFF'"
-        
+         
 print(Check_light_state())
