d4a2fc6f3536f65e21868864968e38cbf20fc29a
kraftedcheese <38340979+kraftedcheese@users.noreply.github.com>
2020-04-02 13:51:30 +0800

    Split init and kv files

--- Kivy/__init__.py
+++ Kivy/__init__.py
@@ -1,1 +1,14 @@
-from Kivy.UI import user_interface
+from kivymd.app import MDApp
+from kivy.lang import Builder
+
+
+class Booked(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.theme_cls.primary_palette = "Teal"
+
+    def build(self):
+        return Builder.load_file("widgets.kv")
+
+
+Booked().run()
